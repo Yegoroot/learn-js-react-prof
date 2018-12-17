70fbe2f9f9b919d62627d026d54dccd782db8dab
Egor Kirsanov <ekirsanov@phoenixit.ru>
2018-12-17 20:19:40 +0300

    костылик

--- src/hoc/protectedRoute.js
+++ src/hoc/protectedRoute.js
@@ -20,6 +20,12 @@ class ProtectedRoute extends Component {
 	}
 }
 
-export default connect(state => ({
-	authorized: !!state[moduleName].user,
-}))(ProtectedRoute)
+export default connect(
+	state => ({
+		authorized: !!state[moduleName].user,
+	}),
+	null,
+	null,
+	{ pure: false },
+)(ProtectedRoute)
+// }))(ProtectedRoute)
