01255d57d1bcdf3ac944b37b0ba4fcfa1feed3dd
Egor Kirsanov <ekirsanov@phoenixit.ru>
2018-12-17 20:48:42 +0300

    reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
 import { reducer as form } from 'redux-form'
-import authReducer from '../ducks/auth'
-import { moduleName as authModule } from '../ducks/auth'
+import authReducer, { moduleName as authModule } from '../ducks/auth'
 
 export default combineReducers({
 	router,
