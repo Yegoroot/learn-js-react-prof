c3cc083bc9238895cd91b7e0126be3a3af412bb3
Egor Kirsanov <ekirsanov@phoenixit.ru>
2018-12-28 16:08:48 +0300

    Добавили инструемнт для ХРОМА REDUX DEV TOOLS

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducer'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
@@ -8,8 +8,14 @@ import createSagaMiddleware from 'redux-saga'
 
 import { saga } from '../ducks/people'
 
+// REDUX TOOLS
+const composeEnhancers =
+	typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+		: compose
+
 const sagaMiddleware = createSagaMiddleware()
-const enhancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), thunk, logger)
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware, routerMiddleware(history), thunk, logger))
 
 const store = createStore(reducer, enhancer)
 
